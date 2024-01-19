a6f3b5b629b7226038a6cf42a4f69be02d6f6557
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-19 13:41:34 +0900

    initial: 초기 프로젝트 설정

---
+++ Swift_AppLifecyle/Swift_AppLifecyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_AppLifecyle
+//
+//  Created by 강창현 on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
